2818fb2e58858e7b24bf2b788e84caf8f1685c5d
harryphma <phamquanghung2705@gmail.com>
2024-12-28 18:14:05 -0700

    adding million.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import MillionLint from "@million/lint";
+import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+ 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [MillionLint.vite(), react()],
+});
